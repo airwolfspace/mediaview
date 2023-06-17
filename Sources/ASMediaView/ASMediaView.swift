6d38604e154571baf7a2db823a5e6733e7cc8274
Kai <kai@v2ex.com>
2023-06-17 13:15:37 +0800

    Add min size to base media view.

--- Sources/ASMediaView/ASMediaView.swift
+++ Sources/ASMediaView/ASMediaView.swift
@@ -5,22 +5,25 @@ struct ASMediaView: View {
     var item: ASMediaItem
 
     var body: some View {
-        if let urls = item.photoURLs {
-            ScrollView(.horizontal, showsIndicators: false) {
-                ForEach(urls, id: \.self) { url in
-                    if let image = NSImage(contentsOf: url) {
-                        Image(nsImage: image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: image.size.width, maxHeight: image.size.height)
+        Group {
+            if let urls = item.photoURLs {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    ForEach(urls, id: \.self) { url in
+                        if let image = NSImage(contentsOf: url) {
+                            Image(nsImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: image.size.width, maxHeight: image.size.height)
+                        }
                     }
                 }
+            } else {
+                VStack {
+                    Text("Hello, Media View.")
+                }
+                .padding()
             }
-        } else {
-            VStack {
-                Text("Hello, Media View.")
-            }
-            .padding()
         }
+        .frame(minWidth: 480, minHeight: 320)
     }
 }
