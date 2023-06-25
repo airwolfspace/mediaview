62855e093246cf29a16e4f0b0edfbc47b619ae3f
Kai <kai@v2ex.com>
2023-06-25 21:49:46 +0800

    Minor updates to last commit.

--- Sources/ASMediaView/ASMediaViewControlBackgroundView.swift
+++ Sources/ASMediaView/ASMediaViewControlBackgroundView.swift
@@ -10,7 +10,7 @@ struct ASMediaViewControlBackgroundView: View {
     
     var body: some View {
         Circle()
-            .fill(.ultraThinMaterial.opacity(isOnHover ? 0.95 : 0.7))
+            .fill(.ultraThinMaterial.opacity(isOnHover ? 0.8 : 0.7))
             .frame(width: 44, height: 44)
             .shadow(color: isOnHover ? Color.secondary.opacity(0.25) : Color.clear, radius: isOnHover ? 1 : 0, x: 0, y: 0)
             .onHover { hovering in
