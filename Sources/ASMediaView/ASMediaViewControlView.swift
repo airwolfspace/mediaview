7a220e313e194bab484b97b4c913b6695b8fc052
Kai <kai@v2ex.com>
2023-06-27 13:36:51 +0800

    Ignore unnecessary hover event.

--- Sources/ASMediaView/ASMediaViewControlView.swift
+++ Sources/ASMediaView/ASMediaViewControlView.swift
@@ -57,11 +57,13 @@ struct ASMediaViewControlView: View {
         .opacity(isHover ? 1.0 : 0.0)
         .onReceive(NotificationCenter.default.publisher(for: .mouseEntered(byID: id)), perform: { _ in
             Task { @MainActor in
+                guard self.isHover == false else { return }
                 self.isHover = true
             }
         })
         .onReceive(NotificationCenter.default.publisher(for: .mouseExited(byID: id)), perform: { _ in
             Task { @MainActor in
+                guard self.isHover == true else { return }
                 self.isHover = false
             }
         })
